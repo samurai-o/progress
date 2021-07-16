54b811b534359cea5c000e258be91a02130b0273
刘伟 <liuwei03@dongfangfuli.com>
2021-07-16 22:09:30 +0800

    api优化

--- packages/progress-core/src/manager.ts
+++ packages/progress-core/src/manager.ts
@@ -52,11 +52,13 @@ export class ProgressCore {
             if (item) {
                 /** 资源进度优先于接口进度样式 */
                 if (item.type === 'fetch' && assestsstatus) {
-                    funcs.find((func) => func.key === 'fetch')?.func(true);
+                    const task = funcs.find((func) => func.key === 'fetch');
+                    if (task && isFunc(task.func)) task.func(true);
                     return this.publish<T>({ ...item, status: true }, func);
                 }
                 if (item.type === 'assets') {
-                    funcs.find((func) => func.key === 'assets')?.func(true);
+                    const task = funcs.find((func) => func.key === 'assets');
+                    if (task && isFunc(task.func)) task.func(true);
                     return this.publish<T>({ ...item, status: true }, func);
                 }
                 return null;
@@ -71,19 +73,19 @@ export class ProgressCore {
         this.tasks = this.tasks.filter((task) => task.id !== item.id && task.name !== item.name && task.type !== item.type);
         const status = this.tasks.filter((task) => task.type === item.type).every((task) => !task.status);
         const func = funcs.find((func) => func.key === item.type);
-        if (!isEmpty(func) && isFunc(func?.func) && status) {
+        if (!isEmpty(func) && isObject(func) && isFunc((func as any).func) && status) {
             // 加载结束时间
             const endTime = new Date().getTime();
             if ((endTime - this.startTime) < 2000 && !this.timeInstance[item.type]) {
                 this.timeInstance[item.type] = setTimeout(() => {
-                    func?.func(false);
+                    (func as any).func(false);
                     clearTimeout(this.timeInstance[item.type] as any);
                     this.timeInstance[item.type] = null;
                 }, 2000);
                 return;
             }
             if (!this.timeInstance[item.type]) {
-                func?.func(false);
+                (func as any).func(false);
                 return;
             }
         }
